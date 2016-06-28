d92fe85904f3afa9eca8a2446ed4913d329e2a87
Balint Erdosi <erdosib@gmail.com>
2016-06-28 18:10:23 +0200

    debugging phase

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 hacking-around
 ==============
 
-Just a collection of the scripts I am using on daily basis – nothing serious, not much interrelated, some marginally interesting stuff. Mostly for myself, for the record, U know...
+For now only home for the Goaticmonkey / httapi Firefox extension. More content to follow, perhaps.
+
--- httapi/index.js
+++ httapi/index.js
@@ -2,42 +2,42 @@ var self = require("sdk/self");
 var querystring = require("sdk/querystring");
 var urls = require("sdk/url");
 var tabs = require("sdk/tabs");
-var panels = require("sdk/panel");
 var { ToggleButton } = require("sdk/ui/button/toggle"); // adding a toolbar button
 var button = ToggleButton({
 	id: "goaticButton",
 	label: "GoaticMonkey",
 	icon: "./goaticon.svg",
-	onClick: showMenu
+	onClick: function(state) {
+		if (state.checked) panel.show({ position: button })
+	}
 })
 
 /* define the panel for the addon*/
-var panel = panels.Panel({
+var panel = require("sdk/panel").Panel({
   contentURL: self.data.url("about:blank"),
-  onHide: handleHide
+  onHide: function() {
+	button.state('window', {checked: false});}
 });
-function showMenu(state){
-	if (state.checked) panel.show({ position: button })
-}
-function handleHide() {
-	button.state('window', {checked: false});
-}
 
 var port = 3000; //whatever is your port
 const {Cc, Ci} = require("chrome");
 var serverSocket = Cc["@mozilla.org/network/server-socket;1"].createInstance(Ci.nsIServerSocket);
+console.log("serversocket created",serverSocket)
 serverSocket.init(port, true, -1);
+console.log("serversocket initiated",serverSocket)
 var listener = {
 	onSocketAccepted: function(socket, transport) {
-		console.log(transport);
+		console.log("transport",transport,"socket",socket);
 		//if ((transport.host == "127.0.0.1") || (transport.host == "0:0:0:0:0:0:0:1") || (transport.host == "::1" )) { 
 			// only accept from localhost...g
 			var input = transport.openInputStream(Ci.nsITransport.OPEN_BLOCKING,0,0);
 			var output = transport.openOutputStream(Ci.nsITransport.OPEN_BLOCKING, 0, 0);
 			var tm = Cc["@mozilla.org/thread-manager;1"].getService();
 			input.asyncWait({
-				onInputStreamReady: function(inp) {
+				onInputStreamReady: function(input) {
+				  console.log("input stream is ready");
 					try {
+									console.log("trying");
 						var sin = Cc["@mozilla.org/scriptableinputstream;1"].createInstance(Ci.nsIScriptableInputStream);
 						sin.init(inp);
 						sin.available();
@@ -80,8 +80,11 @@ var listener = {
 					}
 				}
 			}, 0, 0, tm.mainThread);
+			console.log("input",input,"output",output);
 		//}
 	},
-	onStopListening: function(socket, status) { }
+	onStopListening: function(socket, status) { console.log("stoplistening", socket, status); }
 };
+console.log("Listener ready: ", listener);
 serverSocket.asyncListen(listener);
+console.log("Addon is running.", serverSocket);

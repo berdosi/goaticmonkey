abc6dbe35cdd5dd8ebafbe30c1dae5f20983b33d
Balint Erdosi <erdosib@gmail.com>
2016-07-19 02:14:50 +0200

    Code cleanup.

--- sample/sample2.endpoint.js
+++ sample/sample2.endpoint.js
@@ -1,8 +1,9 @@
-{
+o = {
 	"endpointName": "helloEndpoint47",
-	"allowedURLs": ["hup.hu", "index.hu"]}
+	"allowedURLs": ["hup.hu", "index.hu"]};
 /*body*/
 //alert("API was called.")
+/*eslint-env browser */
 
 Array.prototype.forEach.call(document.querySelectorAll("input"), function(e){e.value = self.options.m; });
-//[... document.querySelectorAll("input")].forEach(function(e){e.value = self.options.m})
+//[... document.querySelectorAll("input")].forEach(function(e){e.value = self.options.m}) 
